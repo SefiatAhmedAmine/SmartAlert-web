bc646b9f196d0151de3f640a59fbb5d467635daa
MohiZzine <farid.mohieddine3@gmail.com>
2024-06-27 22:10:42 +0100

    Small change

--- B2BLink/.next/static/chunks/webpack.js
+++ B2BLink/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a04c07b5a16e979c"; }
+/******/ 		__webpack_require__.h = function() { return "046b00ef8f027461"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- B2BLink/.next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ B2BLink/.next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -87,7 +87,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1ddea25b9f1c572d"; }
+/******/ 	__webpack_require__.h = function() { return "5fff55ff6ef45c3d"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
