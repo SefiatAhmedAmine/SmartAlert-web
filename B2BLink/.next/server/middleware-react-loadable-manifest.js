84e64f1adc680fa6a43450ee164691bc889de69b
unknown <amine-sefiat@hotmail.com>
2024-08-06 02:00:34 +0100

    git ignore .next folder

--- B2BLink/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"_app.js -> ../../public/assets/js/bootstrap.min.js\":{\"id\":\"_app.js -> ../../public/assets/js/bootstrap.min.js\",\"files\":[\"static/chunks/public_assets_js_bootstrap_min_js.js\"]}}"
